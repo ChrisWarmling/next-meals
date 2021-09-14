82370bad29ce71a121d60990bc254a87b8540a7b
Christian Warmling <christianwarmling@gmail.com>
2021-09-14 17:48:16 -0300

    ok

--- src/pages/categorie/[categ].tsx
+++ src/pages/categorie/[categ].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export default function product() {
+export default function Product() {
     const router = useRouter();
 
     return <h1>{router.query.categ}</h1>
--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -14,7 +14,7 @@ interface CategoriesProps{
 
 export const getStaticProps: GetStaticProps<CategoriesProps> = async () => {
     const response = await api.get('/list.php?c=list')
-    const categories = await response.data.meals
+    const categories = response.data.meals
 
     return {
         props:{
@@ -24,7 +24,7 @@ export const getStaticProps: GetStaticProps<CategoriesProps> = async () => {
 }
 
 export default function categories({categories}: CategoriesProps) {
-
+    console.log(categories)
     // useEffect(() => {
     //     api.get('/list.php?c=list').then((response) => {
     //         console.log(response.data.meals)
@@ -38,10 +38,14 @@ export default function categories({categories}: CategoriesProps) {
 
             <section>
                 <ul>
-                    {categories.map((categorie, i) => {
+                    {categories.map((categorie, i) => (
                                 
-                                <li key={i}>{categorie.strCategory}</li>
-                            }
+                                <li key={i}>
+                                    <Link href={`/categorie/${categorie.strCategory}`}>
+                                        <a>{categorie.strCategory}</a>
+                                    </Link>
+                                </li>
+                            )
                         )
                     }
                 </ul>
